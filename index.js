01096e4e9a7b3bcb468b8101c9d1e56781eb0f84
mangalper1 <ivan.galvez.perez@gmail.com>
2019-07-31 16:04:54 +0000

    node modules prueba

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.get("/time", (req, res) => {
+    res.send(new Date());
+})
+
+app.use("/", express.static(__dirname+"/public"));
+
 app.listen(port, () => {
     console.log("Se ha abierto en el puerto "+ port);
 });
