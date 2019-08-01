c52853824bb8502964d2a2c3678fcd30a3f077b7
EC2 Default User <ec2-user@ip-172-31-21-224.ec2.internal>
2019-08-01 17:29:47 +0000

    prueba

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8080;
 
 app.use("/", express.static(__dirname+"/public"));
 app.use(bodyParser.json());
