3c70c1a219b84abf05580aa08652c8ec0a7b5993
EC2 Default User <ec2-user@ip-172-31-21-224.ec2.internal>
2019-08-14 09:29:31 +0000

    l11

--- public/unemployment-rates/minipostman.js
+++ public/unemployment-rates/minipostman.js
@@ -1,3 +1,8 @@
  /*global angular*/
-angular.module("MiniPostmanApp",[]);
+angular.module("MiniPostmanApp",["ngRoute"]).config( function ($routeProvider){
+    $routeProvider.when("/", {
+        controller : "ListCtrl",
+        templateUrl: "list.html"
+    });
+});
 console.log("MiniPostmanApp initialized!");
