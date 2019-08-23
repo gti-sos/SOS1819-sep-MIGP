889fd452ff3beec5dc95d92c3e9d7e12c3742f03
mangalper1 <ivan.galvez.perez@gmail.com>
2019-08-23 09:51:47 +0000

    l13

--- public/integrations/controllers/myAPI-ctrl.js
+++ public/integrations/controllers/myAPI-ctrl.js
@@ -4,21 +4,31 @@ var app = angular.module("App");
 
 app.controller("APICtrl", 
     ["$scope", "$http", "$httpParamSerializer", function($scope, $http, $httpParamSerializer){
+         
+         
+        var API = "https://sos1819-sep-migp.herokuapp.com/api/v1/unemployment-rates";
         
-        
-        var path = "https://sos1819-sep-migp.herokuapp.com/api/v1/unemployment-rates";
-        
-        $http.get(path).then(function(response) {
+        $http.get(API).then(function(response) {
+            
+           var countries = [];
+           var i = 0;
            
+           for(i=0;i<response.length;i++){
+               if(!countries.includes(response.data[i])) {
+                   countries.push(response.data[i]);
+               }
+           }
+           
+           //-------------------------------HIGHCHARTS--------------------------------------------
            Highcharts.chart('container', {
               chart: {
                 type: 'bar'
               },
               title: {
-                text: 'Historic World Population by Region'
+                text: 'Desempleo juvenil por países'
               },
               xAxis: {
-                categories: ['Africa', 'America', 'Asia', 'Europe', 'Oceania'],
+                categories: ["España", "France", "Germany"],
                 title: {
                   text: null
                 }
@@ -26,7 +36,7 @@ app.controller("APICtrl",
               yAxis: {
                 min: 0,
                 title: {
-                  text: 'Population (millions)',
+                  text: 'Desempleo juvenil (porcentaje)',
                   align: 'high'
                 },
                 labels: {
@@ -34,7 +44,7 @@ app.controller("APICtrl",
                 }
               },
               tooltip: {
-                valueSuffix: ' millions'
+                valueSuffix: ' porcentaje '
               },
               plotOptions: {
                 bar: {
@@ -58,19 +68,53 @@ app.controller("APICtrl",
                 enabled: false
               },
               series: [{
-                name: 'Year 1800',
-                data: [107, 31, 635, 203, 2]
+                name: 'Año 2017',
+                data: [response.data.filter(c => c.country == "Spain" && c.year == 2017)[0].youthUnemployment,
+                        response.data.filter(c => c.country == "France" && c.year == 2017)[0].youthUnemployment,
+                        response.data.filter(c => c.country == "Germany" && c.year == 2017)[0].youthUnemployment]
               }, {
-                name: 'Year 1900',
-                data: [133, 156, 947, 408, 6]
-              }, {
-                name: 'Year 2000',
-                data: [814, 841, 3714, 727, 31]
-              }, {
-                name: 'Year 2016',
-                data: [1216, 1001, 4436, 738, 40]
+                name: 'Año 2018',
+                data: [response.data.filter(c => c.country == "Spain" && c.year == 2018)[0].youthUnemployment,
+                        response.data.filter(c => c.country == "France" && c.year == 2018)[0].youthUnemployment,
+                        response.data.filter(c => c.country == "Germany" && c.year == 2018)[0].youthUnemployment]
               }]
             });
-    });
+            
+            //-------------------------------GEOCHART-------------------------------------------
+             google.charts.load('current', {
+                'packages':['geochart'],
+                // Note: you will need to get a mapsApiKey for your project.
+                // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
+                'mapsApiKey': 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY'
+              });
+              google.charts.setOnLoadCallback(drawRegionsMap);
+        
+              function drawRegionsMap() {
+                var data = google.visualization.arrayToDataTable([
+                  ['País', 'Porcentaje de paro'],
+                  [response.data.filter(c => c.country == "Spain" && c.year == "2018")[0].country, response.data.filter(c => c.country == "Spain" && c.year == "2018")[0].rate],
+                  [response.data.filter(c => c.country == "Germany" && c.year == "2018")[0].country, response.data.filter(c => c.country == "Germany" && c.year == "2018")[0].rate],
+                  [response.data.filter(c => c.country == "France" && c.year == "2018")[0].country, response.data.filter(c => c.country == "France" && c.year == "2018")[0].rate],
+                ]);
+        
+                var options = {};
+        
+                var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
+        
+                chart.draw(data, options);
+              }
+              
+              //--------------------------------C3.js--------------------------------------------
+              
+              var chart = c3.generate({
+                    bindto: '#chart',
+                    data: {
+                      columns: [
+                        ['data1', 30, 200, 100, 400, 150, 250],
+                        ['data2', 50, 20, 10, 40, 15, 25]
+                      ]
+                    }
+                });
+        });
 
     }]);
