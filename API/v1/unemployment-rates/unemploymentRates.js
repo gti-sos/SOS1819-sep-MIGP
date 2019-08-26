61ff49b462252e05fe3d1939861093173e2be4aa
mangalper1 <ivan.galvez.perez@gmail.com>
2019-08-26 09:01:50 +0000

    added more stats

--- API/v1/unemployment-rates/unemploymentRates.js
+++ API/v1/unemployment-rates/unemploymentRates.js
@@ -14,53 +14,128 @@ client.connect(err => {
 module.exports = function(app, BASE_PATH){
     var path = "";
     var newUnemploymentRates = [{
-    "country": "Germany",
-    "year": 2018,
-    "rate": 3.3,
-    "youthUnemployment": 15.6,
-    "maleUnemployment": 3.6,
-    "femaleUnemployment": 3.9
-  },
-  {
-    "country": "Spain",
-    "year": 2018,
-    "rate": 14.4,
-    "youthUnemployment": 33.4,
-    "maleUnemployment": 12.7,
-    "femaleUnemployment": 16.2
-  },
-  {
-    "country": "Spain",
-    "year": 2017,
-    "rate": 16.5,
-    "youthUnemployment": 37.1,
-    "maleUnemployment": 15,
-    "femaleUnemployment": 18.3
-  },
-  {
-    "country": "France",
-    "year": 2018,
-    "rate": 8.9,
-    "youthUnemployment": 20.3,
-    "maleUnemployment": 8.8,
-    "femaleUnemployment": 9
-  },
-  {
-    "country": "Germany",
-    "year": 2017,
-    "rate": 3.6,
-    "youthUnemployment": 6.5,
-    "maleUnemployment": 3.9,
-    "femaleUnemployment": 3.1
-  },
-  {
-    "country": "France",
-    "year": 2017,
-    "rate": 9.1,
-    "youthUnemployment": 21.6,
-    "maleUnemployment": 9.1,
-    "femaleUnemployment": 9
-  }];
+        country: "Spain",
+        year: 2018,
+        rate: 14.4,
+        youthUnemployment: 33.4,
+        maleUnemployment: 12.7,
+        femaleUnemployment: 16.2
+        },
+        {
+        country: "Germany",
+        year: 2018,
+        rate: 3.3,
+        youthUnemployment: 15.6,
+        maleUnemployment: 3.6,
+        femaleUnemployment: 3.9
+        },
+        {
+        country: "France",
+        year: 2018,
+        rate: 8.9,
+        youthUnemployment: 20.3,
+        maleUnemployment: 8.8,
+        femaleUnemployment: 9
+        },
+        {
+        country: "UUEE",
+        year: 2018,
+        rate: 3.9,
+        youthUnemployment: 8.7,
+        maleUnemployment: 3.9,
+        femaleUnemployment: 3.8
+        },
+        {
+        country: "UK",
+        year: 2018,
+        rate: 4.0,
+        youthUnemployment: 11.8,
+        maleUnemployment: 4.1,
+        femaleUnemployment: 3.9
+        },
+        {
+        country: "Spain",
+        year: 2016,
+        rate: 16.5,
+        youthUnemployment: 37.1,
+        maleUnemployment: 15,
+        femaleUnemployment: 18.3
+        },
+        {
+        country: "Germany",
+        year: 2017,
+        rate: 3.6,
+        youthUnemployment: 6.5,
+        maleUnemployment: 3.9,
+        femaleUnemployment: 3.1
+        },
+        {
+        country: "France",
+        year: 2017,
+        rate: 9.1,
+        youthUnemployment: 21.6,
+        maleUnemployment: 9.1,
+        femaleUnemployment: 9
+        },
+        {
+        country: "UUEE",
+        year: 2017,
+        rate: 4.1,
+        youthUnemployment: 8.9,
+        maleUnemployment: 4.1,
+        femaleUnemployment: 4.0
+        },
+        {
+        country: "UK",
+        year: 2017,
+        rate: 4.4,
+        youthUnemployment: 12.5,
+        maleUnemployment: 4.4,
+        femaleUnemployment: 4.4
+        },
+        {
+        country: "Spain",
+        year: 2016,
+        rate: 18.5,
+        youthUnemployment: 42.3,
+        maleUnemployment: 17.1,
+        femaleUnemployment: 20.2
+        },
+        {
+        country: "Germany",
+        year: 2016,
+        rate: 3.9,
+        youthUnemployment: 6.7,
+        maleUnemployment: 4.3,
+        femaleUnemployment: 3.4
+        },
+        {
+        country: "France",
+        year: 2016,
+        rate: 9.9,
+        youthUnemployment: 23.2,
+        maleUnemployment: 10.0,
+        femaleUnemployment: 9.8
+        },
+        {
+        country: "UUEE",
+        year: 2016,
+        rate: 4.7,
+        youthUnemployment: 9.8,
+        maleUnemployment: 4.8,
+        femaleUnemployment: 4.6
+        },
+        {
+        country: "UK",
+        year: 2016,
+        rate: 4.8,
+        youthUnemployment: 12.6,
+        maleUnemployment: 4.9,
+        femaleUnemployment: 4.6
+        }
+        
+        
+  ];
 
 
 
